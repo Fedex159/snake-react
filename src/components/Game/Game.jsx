66c6e16ce3f11804501ce8173437555d197b666e
Fedex159 <fede.gallina94@gmail.com>
2022-01-05 16:20:17 -0300

    alternative color

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import s from "./Game.module.css";
+import StartGame from "./StartGame/StartGame";
 
 function Game() {
-  return <div className={s.container}></div>;
+  return (
+    <div className={s.container}>
+      <StartGame />
+    </div>
+  );
 }
 
 export default Game;

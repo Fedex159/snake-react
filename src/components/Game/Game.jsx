8acb0e0cc6ab3254ce78cf0856fe7790c16f29d9
Fedex159 <fede.gallina94@gmail.com>
2022-01-06 11:12:12 -0300

    padding and load board component

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import s from "./Game.module.css";
-import StartGame from "./StartGame/StartGame";
+// import StartGame from "./StartGame/StartGame";
+import Board from "./Board/Board";
 
 function Game() {
   return (
     <div className={s.container}>
-      <StartGame />
+      {/* <StartGame /> */}
+      <Board />
     </div>
   );
 }

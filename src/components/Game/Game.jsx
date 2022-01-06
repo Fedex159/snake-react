21594c05d4ff3526f2625ee40cdbf84ecc8f7def
Fedex159 <fede.gallina94@gmail.com>
2022-01-06 11:37:27 -0300

    add difficult function

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./Game.module.css";
-// import StartGame from "./StartGame/StartGame";
+import StartGame from "./StartGame/StartGame";
 import Board from "./Board/Board";
 
 function Game() {
+  const [difficult, setDifficult] = useState("");
+
   return (
     <div className={s.container}>
-      {/* <StartGame /> */}
-      <Board />
+      {difficult ? (
+        <Board difficult={difficult} />
+      ) : (
+        <StartGame setDifficult={setDifficult} />
+      )}
     </div>
   );
 }
--- src/components/Game/StartGame/StartGame.jsx
+++ src/components/Game/StartGame/StartGame.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import s from "./StartGame.module.css";
 
-const options = ["Easy", "Medium", "Hard"];
+const options = [
+  { name: "Easy", value: 500 },
+  { name: "Medium", value: 200 },
+  { name: "Hard", value: 50 },
+];
 
-function StartGame() {
+function StartGame({ setDifficult }) {
+  const handleClick = (event) => {
+    const value = event.target.id.split("_")[1];
+    setDifficult(value);
+  };
+
   return (
     <div className={s.container}>
       <h2>Snake React</h2>
       <h4>SELECT DIFFICULT:</h4>
       <div className={s.options}>
         {options.map((o, i) => (
-          <span key={`${o}_${i}`}>{o}</span>
+          <span
+            key={`${o.name}_${i}`}
+            onClick={handleClick}
+            id={`${o.name}_${o.value}`}
+          >
+            {o.name}
+          </span>
         ))}
       </div>
     </div>

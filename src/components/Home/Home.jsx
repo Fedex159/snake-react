d3d6fc22729443524b2746c7c52146793ae9a461
Fedex159 <fede.gallina94@gmail.com>
2022-01-07 13:19:02 -0300

    state for manage the audio

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import Game from "../Game/Game";
 import Points from "./Points/Points";
 import Scale from "./Scale/Scale";
+import AudioControl from "./AudioControl/AudioControl";
 import s from "./Home.module.css";
 
 function Home() {
   const [points, setPoints] = useState(0);
   const [maxPoints, setMaxPoints] = useState(0);
   const [showPoints, setShowPoints] = useState(false);
+  const [enableSound, setEnableSound] = useState(true);
 
   return (
     <div className={s.container}>
@@ -16,9 +18,11 @@ function Home() {
           setPoints={setPoints}
           setMaxPoints={setMaxPoints}
           setShowPoints={setShowPoints}
+          enableSound={enableSound}
         />
         {showPoints ? <Points points={points} maxPoints={maxPoints} /> : null}
       </Scale>
+      <AudioControl enableSound={enableSound} setEnableSound={setEnableSound} />
     </div>
   );
 }

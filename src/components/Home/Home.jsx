79e0e83f83a7ae9b4443baa560d79036f70fd36e
Fedex159 <fede.gallina94@gmail.com>
2022-01-07 10:16:59 -0300

    state for manage points

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import s from "./Home.module.css";
+import React, { useState } from "react";
 import Game from "../Game/Game";
+import Points from "./Points/Points";
+import s from "./Home.module.css";
 
 function Home() {
+  const [points, setPoints] = useState(0);
+  const [maxPoints, setMaxPoints] = useState(0);
+  const [showPoints, setShowPoints] = useState(false);
+
   return (
     <div className={s.container}>
-      <Game />
+      <Game
+        setPoints={setPoints}
+        setMaxPoints={setMaxPoints}
+        setShowPoints={setShowPoints}
+      />
+      {showPoints ? <Points points={points} maxPoints={maxPoints} /> : null}
     </div>
   );
 }

e8ca637f18c0b86d93d9064b1af4fb4958594a65
Fedex159 <fede.gallina94@gmail.com>
2022-01-08 19:31:52 -0300

    add key for control volumen

--- src/components/Home/AudioControl/AudioControl.jsx
+++ src/components/Home/AudioControl/AudioControl.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import soundOn from "../../../assets/imgs/soundOn.png";
 import soundOff from "../../../assets/imgs/soundOff.png";
 import { StateGlobal } from "../../Context/Context";
@@ -6,6 +6,21 @@ import s from "./AudioControl.module.css";
 
 function AudioControl() {
   const { enableSound, setEnableSound } = useContext(StateGlobal);
+
+  useEffect(() => {
+    const listener = (event) => {
+      if (event.keyCode === 77) {
+        setEnableSound((prev) => !prev);
+      }
+    };
+
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, [setEnableSound]);
+
   return (
     <div className={s.container}>
       <img

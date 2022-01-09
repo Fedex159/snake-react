a5616fc67fdd0e76a75e5326787abf9c6a2da732
Fedex159 <fede.gallina94@gmail.com>
2022-01-09 10:51:39 -0300

    manage class when key press

--- src/components/Game/ArrowsKeys/ArrowsKeys.jsx
+++ src/components/Game/ArrowsKeys/ArrowsKeys.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import arrowsKeys from "../../../assets/imgs/arrowsKeys.png";
 import s from "./ArrowsKeys.module.css";
 
 const keys = ["ArrowUp", "ArrowDown", "ArrowRight", "ArrowLeft"];
 
-function ArrowsKeys({ handleKey }) {
+function ArrowsKeys({ handleKey, keyOn }) {
+  const [key, setKey] = useState(null);
+
   const handleClick = (event) => {
     if (handleKey) {
       handleKey({ key: event.target.value });
     }
   };
+
+  useEffect(() => {
+    setKey(keyOn);
+  }, [keyOn]);
+
+  useEffect(() => {
+    if (key) {
+      setTimeout(() => {
+        setKey(null);
+      }, [100]);
+    }
+  }, [key]);
 
   return (
     <div
@@ -20,22 +34,22 @@ function ArrowsKeys({ handleKey }) {
     >
       <button
         value={keys[0]}
-        className={`${s.btn} ${s.btnUp}`}
+        className={`${s.btn} ${s.btnUp} ${keys[0] === key && s.btnOn}`}
         onClick={handleClick}
       ></button>
       <button
         value={keys[1]}
-        className={`${s.btn} ${s.btnDown}`}
+        className={`${s.btn} ${s.btnDown} ${keys[1] === key && s.btnOn}`}
         onClick={handleClick}
       ></button>
       <button
         value={keys[2]}
-        className={`${s.btn} ${s.btnRight}`}
+        className={`${s.btn} ${s.btnRight} ${keys[2] === key && s.btnOn}`}
         onClick={handleClick}
       ></button>
       <button
         value={keys[3]}
-        className={`${s.btn} ${s.btnLeft}`}
+        className={`${s.btn} ${s.btnLeft} ${keys[3] === key && s.btnOn}`}
         onClick={handleClick}
       ></button>
     </div>

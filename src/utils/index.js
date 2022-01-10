910eb822f6a9796b6b07833eca97ee3997090178
Fedex159 <fede.gallina94@gmail.com>
2022-01-10 14:12:33 -0300

    add utils por check touch screen

--- src/utils/index.js
+++ src/utils/index.js
@@ -38,3 +38,28 @@ export function getToLocalStorage(key) {
   const value = localStorage.getItem(key);
   return JSON.parse(value);
 }
+
+export function hasTouchScreen() {
+  let hasTouchScreen = false;
+
+  if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+  } else if ("msMaxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+  } else {
+    let mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+    if (mQ && mQ.media === "(pointer:coarse)") {
+      hasTouchScreen = !!mQ.matches;
+    } else if ("orientation" in window) {
+      hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+      // Only as a last resort, fall back to user agent sniffing
+      var UA = navigator.userAgent;
+      hasTouchScreen =
+        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    }
+  }
+
+  return hasTouchScreen;
+}

cd0888ded34f93d59e4cf4d3d8471a6eacad5a63
Fedex159 <fede.gallina94@gmail.com>
2022-01-10 11:26:52 -0300

    load info component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Game from "../Game/Game";
 import Points from "./Points/Points";
 import Scale from "./Scale/Scale";
 import AudioControl from "./AudioControl/AudioControl";
+import InfoButton from "./InfoButton/InfoButton";
+import Info from "./Info/Info";
 import { StateGlobal } from "../Context/Context";
 import s from "./Home.module.css";
 
 function Home() {
   const { showPoints } = useContext(StateGlobal);
+  const [showInfo, setShowInfo] = useState(false);
 
   return (
     <div className={s.container}>
       <Scale>
         <Game />
         {showPoints ? <Points /> : null}
+        {showInfo ? <Info setShowInfo={setShowInfo} /> : null}
       </Scale>
       <AudioControl />
+      <InfoButton setShowInfo={setShowInfo} />
     </div>
   );
 }
